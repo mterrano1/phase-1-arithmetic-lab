7685572a2b4c0f27c1aca039132501c5fc3a3916
Christopher Terrano <mterrano1@yahoo.com>
2021-11-02 12:46:18 -0500

    commit tree e8fba52a2e3a877d84585dd5e61cbf3ffedae291

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let num1 = 31
+
+let num2 = 2
+
+let multiply = (num1 * num2)
+
+let random = Math.ceil(Math.random());
+
+let num3 = 16
+
+let num4 = 12
+
+let mod = (num3 % num4)
+
+let max = Math.max(2, 4, 6, 8, 10, 20)
